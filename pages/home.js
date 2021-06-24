1c0bfe25b2fc75ca1ab00b8ba4b0cdcf13ce58c3
Simon-Oliver <stauffer.simon@outlook.com>
2021-06-24 18:09:17 +0200

    added minimal login form

--- pages/home.js
+++ pages/home.js
@@ -2,10 +2,27 @@ import React from 'react'
 import Link from "next/link"
 
 export default function Home() {
+    const formSubmit = e => {
+        e.preventDefault();
+        console.log(e.target.name.value)
+        console.log(e.target.password.value)
+    }
+
     return (
         <div>
             <h1>Home Page</h1>
             <Link href="/secure">Got to Secure Page</Link>
+            <form onSubmit={formSubmit}>
+                <label>
+                    Username:
+                    <input type="text" name="name" />
+                </label>
+                <label>
+                    Password:
+                    <input type="text" name="password" />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     )
 }
