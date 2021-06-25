5b3f4b1fa5440ae1da17d671a68095fd6798a4bb
Simon-Oliver <stauffer.simon@outlook.com>
2021-06-25 12:44:09 +0200

    exploring firebase auth

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,29 @@
+import { auth } from './config/firebase';
 import React from 'react'
 import Link from "next/link"
 
+
 export default function Home() {
+
+    const signUp = ({ email, password }) => {
+        return auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((response) => {
+                console.log(response)
+            })
+            .catch((error) => {
+                return { error };
+            });
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const data = { email: e.target.name.value, password: e.target.password.value }
+        return signUp(data).then((user) => {
+            console.log(user);
+        });
+    };
+
     const formSubmit = e => {
         e.preventDefault();
         console.log(e.target.name.value)
@@ -12,7 +34,7 @@ export default function Home() {
         <div>
             <h1>Home Page</h1>
             <Link href="/secure">Got to Secure Page</Link>
-            <form onSubmit={formSubmit}>
+            <form onSubmit={onSubmit}>
                 <label>
                     Username:
                     <input type="text" name="name" />
