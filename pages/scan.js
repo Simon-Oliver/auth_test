d5f05cede2d5508249d4e5c0151c9db5cfce7e92
Simon-Oliver <stauffer.simon@outlook.com>
2021-08-10 20:01:41 +0200

    added qr scanner to page

--- pages/scan.js
+++ pages/scan.js
@@ -3,16 +3,19 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useAuth } from './context/authContext'
 import firebase from './config/firebase'
-import InputField from "../components/Input"
-import SubmitForm from "../components/SubmitForm"
+import dynamic from 'next/dynamic'
 import styles from "../styles/home.module.css"
-import QRCode from 'qrcode.react'
+
+const QrReader = dynamic(() => import('react-qr-reader'), { ssr: false })
+
 
 
 export default function scan() {
     const router = useRouter();
     const { authUser, loading, signOut } = useAuth();
     const [userData, setUserData] = useState({ data: {}, error: {}, boxes: [] });
+    const [show, setShow] = useState(true)
+    const [scanData, setScanData] = useState({})
 
     useEffect(() => {
         if (!loading && !authUser) {
@@ -39,10 +42,27 @@ export default function scan() {
         return "";
     }
 
+    const handleScan = data => {
+        if (data) {
+            setScanData(data)
+            setShow(false)
+        }
+    }
+
+    const scan = () => {
+        setScanData({})
+        setShow(true)
+    }
+
 
     return (
         <div>
             <h1>Scan</h1>
+            {show ? <QrReader className={styles.scanner} onScan={handleScan} onError={() => console.log('error')} /> :
+                <>
+                    <p>{scanData}</p>
+                    <button onClick={scan}>Scan</button>
+                </>}
         </div>
     )
 }
