d2267eee02684402ad860dbd5e13b48941ebef43
Simon-Oliver <stauffer.simon@outlook.com>
2021-09-24 20:47:50 +1000

    added styling to card

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,22 +1,23 @@
-import React, { useState } from 'react';
-import styles from "./Card.module.css"
+import React, { useState } from "react";
+import styles from "./Card.module.css";
 
 export default function Card(props) {
-    const [isShowing, setIsShowing] = useState(false);
+  const [isShowing, setIsShowing] = useState(false);
 
-    const toggle = () => {
-        setIsShowing(!isShowing);
-    };
+  const toggle = () => {
+    setIsShowing(!isShowing);
+  };
 
-    console.log(props);
-    return (
-
-        <div className={styles.card} onClick={toggle}>
-            <h3>{props.title}</h3>
-            <div className={isShowing ? `${styles.panel} ${styles.show}` : styles.panel}>
-                <p> {props.content}</p>
-                {props.children}
-            </div>
-        </div >
-    )
+  console.log(props);
+  return (
+    <div className={styles.card} onClick={toggle}>
+      <h4 className={styles.title}>{props.title}</h4>
+      <div
+        className={isShowing ? `${styles.panel} ${styles.show}` : styles.panel}
+      >
+        <p> {props.content}</p>
+        {props.children}
+      </div>
+    </div>
+  );
 }
--- pages/home.js
+++ pages/home.js
@@ -1,75 +1,82 @@
-import React, { useEffect } from 'react'
-import Link from "next/link"
-import { useAuth } from './context/authContext'
-import { useRouter } from 'next/router';
+import React, { useEffect } from "react";
+import Link from "next/link";
+import { useAuth } from "./context/authContext";
+import { useRouter } from "next/router";
 import Login from "../components/Login";
-import styles from '../styles/home.module.css'
-
-
-
+import styles from "../styles/home.module.css";
 
 export default function Home() {
-    const { authUser, loading, createUserWithEmailAndPassword, signInWithEmailAndPassword } = useAuth();
-    const router = useRouter()
+  const {
+    authUser,
+    loading,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+  } = useAuth();
+  const router = useRouter();
 
-    useEffect(() => {
-        if (!loading && authUser) {
-            console.log(authUser, loading)
-            router.push('/loggedin')
-        }
-
-    }, [authUser, loading])
-
-    if (loading || authUser) {
-        return ""
+  useEffect(() => {
+    if (!loading && authUser) {
+      console.log(authUser, loading);
+      router.push("/loggedin");
     }
-    const signUp = ({ email, password }) => {
-        return createUserWithEmailAndPassword(email, password)
-            .then((response) => {
-                console.log(response)
-                router.push("/loggedin")
-            })
-            .catch((error) => {
-                return { error };
-            });
+  }, [authUser, loading]);
+
+  if (loading || authUser) {
+    return "";
+  }
+  const signUp = ({ email, password }) => {
+    return createUserWithEmailAndPassword(email, password)
+      .then((response) => {
+        console.log(response);
+        router.push("/loggedin");
+      })
+      .catch((error) => {
+        return { error };
+      });
+  };
+
+  const signIn = ({ email, password }) => {
+    return signInWithEmailAndPassword(email, password)
+      .then((response) => {
+        console.log(response);
+        router.push("/loggedin");
+      })
+      .catch((error) => {
+        return { error };
+      });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      email: e.target.name.value,
+      password: e.target.password.value,
     };
+    return signUp(data).then((user) => {
+      console.log(user);
+    });
+  };
 
-    const signIn = ({ email, password }) => {
-        return signInWithEmailAndPassword(email, password)
-            .then((response) => {
-                console.log(response)
-                router.push("/loggedin")
-            })
-            .catch((error) => {
-                return { error };
-            });
+  const onSubmitSign = (e) => {
+    e.preventDefault();
+    const data = {
+      email: e.target.name.value,
+      password: e.target.password.value,
     };
+    return signIn(data).then((user) => {
+      console.log(user);
+    });
+  };
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        const data = { email: e.target.name.value, password: e.target.password.value }
-        return signUp(data).then((user) => {
-            console.log(user);
-        });
-    };
+  const formSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.name.value);
+    console.log(e.target.password.value);
+  };
 
-    const onSubmitSign = (e) => {
-        e.preventDefault();
-        const data = { email: e.target.name.value, password: e.target.password.value }
-        return signIn(data).then((user) => {
-            console.log(user);
-        });
-    };
-
-    const formSubmit = e => {
-        e.preventDefault();
-        console.log(e.target.name.value)
-        console.log(e.target.password.value)
-    }
-
-    return (
-        <div className={styles.body}>
-            <Login></Login>
-        </div>
-    )
+  return (
+    <div className={styles.body}>
+      <Login></Login>
+    </div>
+  );
 }
